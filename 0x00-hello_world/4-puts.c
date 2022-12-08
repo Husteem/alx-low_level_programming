01f8b5cc2cb952a2243f30e83989e32cfd8244a3
Husteem <muhdnaimhussain@gmail.com>
2022-12-08 15:12:57 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  *  * Description: main - Puts the texts to stdout.
- *   * Return: 0 if succesfull.
+ *   * Return: 0 should code runs successfully.
  *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
