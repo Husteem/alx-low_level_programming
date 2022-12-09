b3b04c05e80fb54168d024901273664cf5f80d34
Husteem <muhdnaimhussain@gmail.com>
2022-12-09 15:01:15 -0800

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 /**
- * main-program entry point.
- * Return:0 no error, non zero if error
+ * Description: main - A program that prints all single digit
+ * You are not allowed to use any variable of type char
+ * Return: 0
  */
+
 int main(void)
 {
-int i;
-}
-}
-for (i = '0'; i <= '9'; i++)
-putchar(i);
+int num;
+for (num = "0"; num <= "9"; num++)
+putchar(num);
 putchar('\n');
 return (0);
 }
