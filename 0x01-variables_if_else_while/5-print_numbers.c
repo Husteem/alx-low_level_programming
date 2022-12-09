9df80bc6092606cd8b4b88a0f8dea5170aab4945
Husteem <muhdnaimhussain@gmail.com>
2022-12-09 13:31:20 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Description: main - A program that prints all single digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	while (num < 10)
+	{
+		printf("%d", num);
+		num += 1;
+	}
+	printf("\n");
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
