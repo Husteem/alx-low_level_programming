eec9a5019c3ee37acbe443ad2cc263a84920a13d
Husteem <muhdnaimhussain@gmail.com>
2022-12-09 14:49:29 -0800

    03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int slts = 'a';
-	int caps = 'A';
-	while (slts <= 'z')
-	{
-		putchar(slts);
-		slts += 1;
-	}
-	while (caps <= 'Z')
-	{
-		putchar(caps);
-		caps += 1;
-	}
-	putchar('\n');
-	return (0);
+int slts = 'a';
+int caps = 'A';
+while (slts <= 'z')
+{
+putchar(slts);
+slts += 1;
+}
+while (caps <= 'Z')
+{
+putchar(caps);
+caps += 1;
+}
+putchar('\n');
+return (0);
 }
