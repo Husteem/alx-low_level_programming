bfb876f78f8f5cdc78c1fbcf6d7f8972422125b8
Husteem <muhdnaimhussain@gmail.com>
2022-12-13 16:37:51 -0800

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
-prod = num * mult;
+prod num * mult;
 if (prod <= 99)
 _putchar(' ');
 if (prod <= 9)
@@ -28,11 +28,12 @@ _putchar((prod / 100) + '0');
 _putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
-}
+{
 _putchar((prod / 10) + '0');
 }
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
-
+}
+}
