011591a1b5021ef50e6a91ba7759aac52873b3b9
Husteem <muhdnaimhussain@gmail.com>
2022-12-13 16:23:03 -0800

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,27 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- * separated by a comma followed by a space.
- * Return: Always 0.
- * husteemah
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
-int main(void)
+int _islower(int c)
 {
-innt count;
-unsigned long fib1 = 0, fib2 = 1, sum;
-for (count = 0; count < 50; count++)
-{
-sum = fib1 + fib2;
-printf("%lu", sum);
-fib1 = fib2;
-fib2 = sum;
-
-if (count == 49)
-printf("\n");
+if (c >= 'a' && c <= 'z')
+return (1);
 else
-printf(", ");
-}
-
 return (0);
 }
