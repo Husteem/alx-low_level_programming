30771fbe7a0ae555c361ce1c438b110a9092e11d
Husteem <muhdnaimhussain@gmail.com>
2022-12-15 15:10:00 -0800

    3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to check.
+ * Description: checks for a digit.
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+i((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first num
+ * @b: second num
+ * Description: multiplies two integers.
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
