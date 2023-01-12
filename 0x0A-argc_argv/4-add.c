0bf89a23319558904f48a569ce5cefccda84749c
Husteem <muhdnaimhussain@gmail.com>
2023-01-12 12:47:28 -0800

    To infinity and beyond, Toy story

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,10 +4,10 @@
 #include "main.h"
 
 /**
- *  * main - a program that adds positive numbers.
- *   * @argc: argument count.
- *    * @argv: argument vector arrays.
- *     * Return: EXIT_SUCCESS.
+ * main - a program that adds positive numbers.
+ * @argc: argument count.
+ * @argv: argument vector arrays.
+ * Return: EXIT_SUCCESS.
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
@@ -15,8 +15,9 @@ int main(int argc __attribute__((unused)), char *argv[])
 	int sum;
 	int count;
 	int x;
-count = 1;
-sum = 0;
+
+	count = 1;
+	sum = 0;
 	if (argc == 1)
 	{
 		printf("0\n");
@@ -29,12 +30,12 @@ sum = 0;
 			if (!(isdigit(argv[count][x])))
 			{
 				printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[count]);
-count++;
-}
-printf("%d\n", sum);
-return (0);
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
