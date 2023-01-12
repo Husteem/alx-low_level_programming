19e5df64db047f22b4c619f3d6d9b13bf7f72169
Husteem <muhdnaimhussain@gmail.com>
2023-01-12 12:39:47 -0800

    It isn't the mountains ahead to climb that wear you out

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,39 +12,40 @@
 
 char *argstostr(int ac, char **av)
 {
-char *str;
-int x, y, z, ln;
-if (ac == 0 || av == NULL)
-return (NULL);
-x = ln = 0;
-while (x < ac)
-{
-y = 0;
-while (av[x][y] != '\0')
-{
-ln++;
-y++;
-}
-ln++;
-x++;
-}
-ln++;
-str = malloc(ln * sizeof(char));
-if (str == NULL)
-return (NULL);
-x = z = 0;
-while (x < ac)
-{
-y = 0;
-while (av[x][y] != 0)
-{
-str[z] = av[x][y];
-z++;
-y++;
-}
-str[z] = '\n';
-z++;
-x++;
-}
-return (str);
+	char *str;
+	int x, y, z, ln;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	x = ln = 0;
+	while (x < ac)
+	{
+		y = 0;
+		while (av[x][y] != '\0')
+		{
+			ln++;
+			y++;
+		}
+		ln++;
+		x++;
+	}
+	ln++;
+	str = malloc(ln * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	x = z = 0;
+	while (x < ac)
+	{
+		y = 0;
+		while (av[x][y] != 0)
+		{
+			str[z] = av[x][y];
+			z++;
+			y++;
+		}
+		str[z] = '\n';
+		z++;
+		x++;
+	}
+	return (str);
 }
